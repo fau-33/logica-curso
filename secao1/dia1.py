94a6b9b80710c3a7d159d8a6a35f5b89b29fbc46
Flávio Félix <fau_felix30@hotmail.com>
2024-12-10 11:13:58 -0300

    Começando curso de logica mente

---
+++ secao1/dia1.py
@@ -0,0 +1,6 @@
+#Preparar um café
+#Pegue a panela de fogo, leve para o forno, coloque o café e espere 5 minutos
+#depois, retire do forno e deixe esfregar por 10 minutos
+#e em seguida, sirva
+
+print("Bem vindo ao método mente lógica! Flávio");
